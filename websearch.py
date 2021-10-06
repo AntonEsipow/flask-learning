488d8809a4fae6575bd421b8a57bd627fb33e137
AntonEsipow <esipow.anton99@gmail.com>
2021-10-06 19:45:02 +0300

    First flask commit

---
+++ websearch.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from vsearch import search_for_letters
+
+app = Flask(__name__)
+
+
+@app.route('/search', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search_for_letters(phrase, letters))
+
+    title = "Results of searching"
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+
+app.run(debug=True)
